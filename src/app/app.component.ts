9ce4d7e2306d240faaacf7de7125b3c308501b73
Sain Marbaniang <88076344+TerzaSlayer2000@users.noreply.github.com>
2023-09-01 14:45:03 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCalculator';
+  input: string = '';
+  result: string = '';
+
+  clear()
+  {
+    this.input=''; 
+    this.result='';
+  }
+
+  numberFunc(number: string){
+    if(this.input == ''){
+      if(number == '.')
+      {
+        return;
+      }
+      if(number == '0')
+      {
+        return;
+      }
+    }
+    this.input = this.input+number;
+  }
+
+  operatorFunc(operator: string){
+    if(this.input!='')
+    {
+      this.input=this.input+operator;
+    }
+    else{
+      if(operator == '-')
+      {
+        this.input = this.input+operator;
+      }
+    }
+    
+  }
+
+  answer()
+  {
+    this.result=eval(this.input);
+
+  }
+
+  delFunc()
+  {
+    if(this.input!=""){
+      this.input=this.input.substr(0,this.input.length-1);
+      this.result='';
+    }
+    
+  }
 }
